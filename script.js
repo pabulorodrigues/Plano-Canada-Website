2cfc5bf08cc9295d4a10805591ebb5b815542e9b
Pabulo Rodrigues <pabulor9@gmail.com>
2022-01-20 02:52:10 -0500

    :package: "upadate layout"

--- script.js
+++ script.js
@@ -1,5 +1,49 @@
-;
+console.log('Hello!')
 
-document.querySelector(".hamburguer").addEventListener("click", () =>
+document.querySelector('.navbar');
+
+window.onscroll = function(){
+    var top = window.scrollY;
+    console.log(top);
+    if (top >= 17){
+        document.querySelector(".container").classList.add('show-menu')
+    }else{
+        document.querySelector(".container").classList.remove('show-menu');
+    } 
+
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+/*
+
+.addEventListener("click", () =>
     document.querySelector(".container").classList.toggle("show-menu")
-);
+); 
+
+*/
