e2de487fc4cb99e60301074bf5e82a9b2ad037c5
Pabulo Rodrigues <pabulor9@gmail.com>
2022-01-20 22:37:29 -0500

    :update: "Upadate Layout"

--- script.js
+++ script.js
@@ -6,13 +6,16 @@ window.onscroll = function(){
     var top = window.scrollY;
     console.log(top);
     if (top >= 17){
-        document.querySelector(".container").classList.add('show-menu')
+        document.querySelector(".container").classList.add('change-color')
     }else{
-        document.querySelector(".container").classList.remove('show-menu');
+        document.querySelector(".container").classList.remove('change-color');
     } 
 
 }
 
+document.querySelector(".menu-social").addEventListener("click", () =>
+ document.querySelector(".container").classList.toggle("show-social")
+); 
 
 
 
