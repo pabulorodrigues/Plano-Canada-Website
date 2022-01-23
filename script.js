e47752df575e311ca62d1129d1f6b48064777620
Pabulo Rodrigues <pabulor9@gmail.com>
2022-01-22 23:40:47 -0500

    :package: "section feedback"

--- script.js
+++ script.js
@@ -17,6 +17,64 @@ document.querySelector(".menu-social").addEventListener("click", () =>
  document.querySelector(".container").classList.toggle("show-social")
 ); 
 
+ 
+
+
+document.body.onresize = function() {
+    if (document.body.clientWidth <= 1003) {
+        document.querySelector(".container").classList.add("rize-hamburguer")
+    }else{
+        document.querySelector(".container").classList.remove("rize-hamburguer")
+        document.querySelector(".container").classList.remove("show-menu");
+    }
+};
+
+window.onload = function() {
+    if (document.body.clientWidth <= 1000) {
+        document.querySelector(".container").classList.add("rize-hamburguer")
+    }else{
+        document.querySelector(".container").classList.remove("rize-hamburguer")
+    }
+  };
+
+
+
+document.querySelector(".hamburguer").addEventListener("click", () =>
+ document.querySelector(".container").classList.toggle("show-menu")
+
+);
+
+document.querySelector(".img-wrapper").addEventListener("click", () =>
+ document.querySelector(".container").classList.remove("show-menu")
+
+);
+
+document.querySelector(".navbar").addEventListener("click", () =>
+ document.querySelector(".container").classList.remove("show-menu")
+
+);
+
+ document.querySelector(".banner").addEventListener("click", () =>
+ document.querySelector(".container").classList.remove("show-menu")
+
+ );
+
+ /*.addEventListener("click", () =>*/
+
+ /*
+document.body.onresize = function() {
+    if (document.body.clientWidth < 1000) {
+        document.querySelector(".hamburguer").classList.add("show-hamburguer")
+    }else{
+        document.querySelector(".hamburguer").classList.remove("show-hamburguer");
+    } 
+
+}; */
+    
+
+
+
+
 
 
 
