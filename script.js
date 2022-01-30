acb59170f12139f501b78ac377c3abd70a7e82ae
Pabulo Rodrigues <pabulor9@gmail.com>
2022-01-29 19:44:46 -0500

    :package: "translation"

--- script.js
+++ script.js
@@ -18,9 +18,6 @@ window.onload = function() {
     }
   };
 
-
-
-
 document.querySelector(".hamburguer").addEventListener("click", () =>
  document.querySelector(".container").classList.toggle("show-menu")
 
@@ -35,6 +32,29 @@ document.querySelector(".img-wrapper").addEventListener("click", () =>
  document.querySelector(".container").classList.remove("show-menu")
 
  );
+
+const flagsElement = document.getElementById("flags");
+
+const textsToChange = document.querySelectorAll("[data-section]");
+
+const changeLanguage = async (language) => {
+    const requestJson = await fetch(`./languages/${language}.json`);
+    const texts = await requestJson.json();
+
+    for (const textToChange of textsToChange) {
+        const section = textToChange.dataset.section;
+        const value = textToChange.dataset.value;
+
+
+        textToChange.innerHTML = texts[section][value];
+
+    }
+};
+
+flagsElement.addEventListener("click", (e) => {
+    changeLanguage(e.target.parentElement.dataset.language);
+});
+
 
  
 
